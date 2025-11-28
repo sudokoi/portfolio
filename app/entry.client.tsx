fce5b4cae971cef40d43184f418fca5ffcccd7f9
Sudhanshu <perfectsudh@gmail.com>
2025-11-28 14:12:44 +0530

    feat: integrate sentry for monitoring

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,12 +1,30 @@
+import { init, browserTracingIntegration } from "@sentry/remix";
 /**
  * By default, Remix will handle hydrating your app on the client for you.
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import { RemixBrowser, useLocation, useMatches } from "@remix-run/react";
+import { startTransition, StrictMode, useEffect } from "react";
 import { hydrateRoot } from "react-dom/client";
+
+init({
+  dsn: "https://5f711f23dca8cae55c3ef1c0dde80b4e@o4507639875239936.ingest.us.sentry.io/4507639877992448",
+  tracesSampleRate: 1,
+  enableLogs: true,
+  tunnel: "/tunnel",
+
+  integrations: [
+    browserTracingIntegration({
+      useEffect,
+      useLocation,
+      useMatches,
+    }),
+  ],
+
+  sendDefaultPii: true,
+});
 
 startTransition(() => {
   hydrateRoot(
